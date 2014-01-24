75261495e4623c474f0ccc5c548c139db7d48547
Chris Klosowski <cklosowski@gmail.com>
2014-01-23 20:38:29 -0700

    Adding GitHub Updater Support https://github.com/afragen/github-updater

--- inspiring-dashboards.php
+++ inspiring-dashboards.php
@@ -7,6 +7,7 @@ Version: 1.0
 Author: Chris Klosowski
 Author URI: http://kungfugrep.com
 License: GPLv2
+GitHub Plugin URI: https://github.com/cklosowski/inspiring-dashboards
 */
 
 function ck_inspiring_dashboards_register() {
